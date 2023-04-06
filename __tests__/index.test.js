150d69b919a9807811f0a3610e3d85c27cc06ef6
Marianna Ikonomou <ma.ikonomou@hotmail.com>
2023-04-06 12:05:02 +0200

    test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,41 +1,12 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Home from "./Home";
+import Home from "../pages";
+import { render, screen } from "@testing-library/react";
 
 describe("Home", () => {
-  it("renders without errors", () => {
+  test("renders heading", () => {
     render(<Home />);
-  });
-
-  it("displays submission information when there are submissions", () => {
-    const submissions = [
-      {
-        lostLocated: "Lost",
-        name: "Fluffy",
-        description: "Small white dog with brown spots",
-        contact: "555-1234",
-      },
-    ];
-
-    const { getByText } = render(<Home />, {
-      wrapper: ({ children }) => (
-        <StateContext.Provider value={[submissions]}>
-          {children}
-        </StateContext.Provider>
-      ),
+    const headingElement = screen.getByRole("heading", {
+      name: "Find my Pet (working title)",
     });
-
-    expect(getByText("Fluffy")).toBeInTheDocument();
-    expect(getByText("Small white dog with brown spots")).toBeInTheDocument();
-    expect(getByText("Contact: 555-1234")).toBeInTheDocument();
-  });
-
-  it("does not display submission information when there are no submissions", () => {
-    const { queryByText } = render(<Home />);
-    expect(queryByText("Fluffy")).not.toBeInTheDocument();
-    expect(
-      queryByText("Small white dog with brown spots")
-    ).not.toBeInTheDocument();
-    expect(queryByText("Contact: 555-1234")).not.toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
   });
 });
