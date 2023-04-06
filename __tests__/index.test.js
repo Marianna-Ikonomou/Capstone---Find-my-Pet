a4a9190676e0f3be5cc5bd2829dece215712bc52
Marianna Ikonomou <ma.ikonomou@hotmail.com>
2023-04-06 12:41:12 +0200

    pass a test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,32 @@
-import Home from "../pages";
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../pages/index";
+import { StateContext } from "../context/state.js";
 
-describe("Home", () => {
-  test("renders heading", () => {
-    render(<Home />);
-    const headingElement = screen.getByRole("heading", {
-      name: "Find my Pet (working title)",
+describe("Home component", () => {
+  it("renders the heading", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Find my Pet (working title)")).toBeInTheDocument();
+  });
+
+  it("renders the list of submissions", () => {
+    const submissions = [
+      {
+        lostLocated: "Lost",
+      },
+      {
+        lostLocated: "Located",
+      },
+    ];
+
+    const { getByText } = render(
+      <StateContext.Provider value={[submissions]}>
+        <Home />
+      </StateContext.Provider>
+    );
+
+    submissions.forEach((submission) => {
+      expect(getByText(submission.lostLocated)).toBeInTheDocument();
     });
-    expect(headingElement).toBeInTheDocument();
   });
 });
