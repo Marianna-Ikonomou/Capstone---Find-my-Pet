6950d39bb02c15c844b3382a091f01dfebf10232
Marianna Ikonomou <ma.ikonomou@hotmail.com>
2023-04-13 22:30:14 +0200

    add download button

--- components/Form/index.js
+++ components/Form/index.js
@@ -3,6 +3,16 @@ import { useState } from "react";
 import React, { useContext } from "react";
 import { CldImage, CldUploadButton } from "next-cloudinary";
 import { StateContext } from "../../context/state.js";
+import Image from "next/image.js";
+import {
+  PDFDownloadLink,
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image as PDFImage,
+} from "@react-pdf/renderer";
 
 export default function PetForm({ onSubmit }) {
   const [photoUrl, setPhotoUrl] = useState(null);
@@ -13,6 +23,82 @@ export default function PetForm({ onSubmit }) {
   const [description, setDescription] = useState(null);
   const [contact, setContact] = useState(null);
   const [location, setLocation] = useState(null);
+
+  const [showPDFDownloadLink, setShowPDFDownloadLink] = useState(false);
+
+  const generatePDF = (data) => {
+    const styles = StyleSheet.create({
+      page: {
+        flexDirection: "column",
+        padding: 10,
+        textAlign: "center",
+      },
+      section: {
+        margin: 10,
+        padding: 10,
+      },
+      heading: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 10,
+      },
+      label: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 5,
+      },
+      text: {
+        fontSize: 14,
+        marginBottom: 10,
+      },
+      input: {
+        marginBottom: 20,
+        padding: 5,
+        borderRadius: 5,
+        border: "1px solid #ccc",
+        fontSize: 14,
+      },
+      image: {},
+    });
+
+    const photoCloudinaryUrl = publicId
+      ? `https://res.cloudinary.com/your_cloud_name/image/upload/${publicId}`
+      : null;
+
+    const pdfDoc = (
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text style={styles.heading}>
+              {document.getElementById("lostLocated").value}
+            </Text>
+
+            {photoCloudinaryUrl && (
+              <PDFImage
+                style={{ marginBottom: 10 }}
+                src={photoCloudinaryUrl}
+                alt="Pet"
+              />
+            )}
+
+            <Text style={styles.text}>
+              {document.getElementById("name").value}
+            </Text>
+
+            <Text style={styles.text}>
+              {document.getElementById("description").value}
+            </Text>
+            <Text style={styles.label}>Contact Information:</Text>
+            <Text style={styles.text}>
+              {document.getElementById("contact").value}
+            </Text>
+          </View>
+        </Page>
+      </Document>
+    );
+
+    return pdfDoc;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -27,6 +113,7 @@ export default function PetForm({ onSubmit }) {
     };
     setSubmissions([...submissions, newSubmission]);
     onSubmit(newSubmission);
+    setShowPDFDownloadLink(true);
   };
   const handleDelete = (index) => {
     const newSubmissions = [...submissions];
@@ -137,6 +224,14 @@ export default function PetForm({ onSubmit }) {
               </p>
               <p>Contact: {submission.contact}</p>
               <button onClick={() => handleDelete(index)}>Delete</button>
+              {showPDFDownloadLink && (
+                <PDFDownloadLink
+                  document={generatePDF()}
+                  fileName="pet-form.pdf"
+                >
+                  Download PDF
+                </PDFDownloadLink>
+              )}
             </li>
           ))}
         </ul>
